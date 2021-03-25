63059689b0ad93c14884e1a7f3977ef62958a3b8
nochi <marcocesana98@gmail.com>
2021-03-25 18:13:25 +0100

    Changed name property of the application

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'name' => 'Scopus4fp7',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',

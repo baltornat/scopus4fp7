943602b03cb8b61506ed52c3f8a5f747c973bdad
nochi <marcocesana98@gmail.com>
2021-03-25 01:22:09 +0100

    Added access control in AuthorsProjectPpiController (not the final version)

--- controllers/AuthorsProjectPpiController.php
+++ controllers/AuthorsProjectPpiController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\AuthorsProjectPpi;
 use app\models\AuthorsProjectPpiSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,17 @@ class AuthorsProjectPpiController extends Controller
     public function behaviors()
     {
         return [
+            [
+                'class' => AccessControl::class,
+                'only' => ['index', 'view', 'create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'actions' => ['index', 'view', 'create', 'update', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [

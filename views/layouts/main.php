62dbe40677af8e65c078f85564ebe1262e18d516
nochi <marcocesana98@gmail.com>
2021-03-29 16:54:47 +0200

    Updated footer style

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -65,7 +65,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Università degli Studi di Milano - <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
